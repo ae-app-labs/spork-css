ef2c7d22744c2a4b651f9856fbda616d9c1408f8
midhunhk <2026817+midhunhk@users.noreply.github.com>
2023-04-21 13:57:28 -0400

    restructure folders

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,16 @@ const sass = require('gulp-sass')(require('sass'))
 const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
-    return src('sass/**/*.scss')
+    return src('test/sass/**/*.scss')
         .pipe(sass())
         .pipe(purgecss({
-            content:['*.html']
+            content:['test/*.html']
         }))
-        .pipe(dest('css'))
+        .pipe(dest('test/css'))
 }
 
 function watchTask() {
-    watch(['sass/**/*.scss', '*.html'], buildStyles)
+    watch(['test/sass/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
